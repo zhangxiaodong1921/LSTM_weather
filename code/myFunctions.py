5c63e1b5dcce6d34aca0cb9144a1fda1cdb9b3ca
Leonard Rychly <leonardrychly@gmx.net>
2017-08-04 08:33:29 +0200

    new weather lstm

--- code/myFunctions.py
+++ code/myFunctions.py
@@ -1,17 +1,49 @@
 import os
 import sys
 import numpy as np
-import pandas
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, normalize, StandardScaler
 
 
 
-def read_data():
-    dataDir = "/Users/leonardrychly/Dropbox/Projects/Weather Fusion/"\
-               "LSTM_weather/data/"
-    fname = 'international-airline-passengers.csv'
-    dataset = pandas.read_csv(dataDir + fname, usecols=[1], engine='python', 
-                              skipfooter=3)
-    return dataset
+def read_data(dataset, processing='norm'):
+    """ Read in data and normalize """
+    if dataset == 'lstm_toy':
+        dataDir = "/Users/leonardrychly/Dropbox/Projects/Weather Fusion/"\
+                   "LSTM_weather/data/"
+        fname = 'international-airline-passengers.csv'
+        dataset = pd.read_csv(dataDir + fname, usecols=[1], engine='python', 
+                                  skipfooter=3)
+        return dataset
+    elif dataset == 'sanmateo':
+        dataDir = "/Users/leonardrychly/Dropbox/Projects/Weather Fusion/"\
+                  "LSTM_weather/data/"
+        fname = 'q_rec.csv'
+        # read data
+        dataset = pd.read_csv(dataDir + fname)
+        datetimeData = pd.to_datetime(dataset['Date Time'].values)\
+                       .to_pydatetime()
+        dtData = np.asarray([i.timestamp() for i in datetimeData])
+        tempData = dataset['TempOutside [C]'].values
+        humData = dataset['HumInside [%]'].values
+        #dataset = np.vstack((dtData,tempData, humData))
+        dataset = tempData.reshape(-1, 1)
+        # normalize data with data: (n_channels,n_samples)
+        if processing == 'norm':
+            dataset = normalize(dataset)
+            return dataset.T, 0
+        elif processing == 'std':
+            scaler = StandardScaler().fit(dataset.T) #T
+            dataset = scaler.transform(dataset.T) #T
+            return dataset, scaler
+        elif processing == 'minmax':
+            scaler = MinMaxScaler(feature_range=(0, 1)).fit(dataset) #T
+            dataset = scaler.transform(dataset) #T
+            return dataset, scaler
+        elif processing == 'none':
+            return dataset, 0
+
+
 
 
 def create_dataset(dataset, look_back=1):
